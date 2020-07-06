e7cb59e15e1f2308dfeaa8767ff4ab104c9dfbc8
Osner Sanchez <osner.sanchez@gmail.com>
2020-07-06 00:18:26 -0500

    delete dependency

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,5 +61,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
